338b295152fbb5db9a759965e9d50e9f9e3d31f4
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-10-15 16:03:42 -0700

    added meta tags and changed Title for SEO

--- frontend/src/app/skills/skills.component.ts
+++ frontend/src/app/skills/skills.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, AfterViewInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, AfterViewInit, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
@@ -7,28 +7,26 @@ import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef, AfterViewI
   encapsulation: ViewEncapsulation.Emulated
 })
 
-
+@Injectable({
+  providedIn: "root"
+})
 export class SkillsComponent implements OnInit, AfterViewInit {
   // to get the height of content
   @ViewChild('content') anyName!: ElementRef
-  // to send the height of the content
-  @Output() heightSend = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit() {
     // gets the height of the page
     let pageHeight = this.anyName.nativeElement.clientHeight
     console.log("page content height: " + pageHeight)
-
-    // sends page height to route-animations.ts
-    const heightSender = () => this.heightSend.emit(pageHeight)
-    heightSender
-
+    return pageHeight
   }
+  // console.log(ngAfterViewInit: any)
 
 }
 
